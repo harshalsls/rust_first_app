77f56b3f6c631eef0814e04099e109801b7b5db1
harshaltrux <hshinde@truxnow.com>
2024-01-07 13:55:25 +0530

    variables and function

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,22 @@ fn main() {
     println!("Hello, world! This is Harshal");
     let x = 1;
 
+    let two: i32  = 2;
+    let hello = "hello";
+    let j = 'j';
+    let my_half = 0.5;
+    let my_name = "Bil";
+    let quit_program = false;
+    let your_half = my_half;
+
+    let x = add(1,1);
+    let y = add(3,0);
+    let z = add(x, 1);
+    println!("{}", z);
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    let x = (a + b)/2;
+    return x;
+}
+
